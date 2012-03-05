32cd7df98307c86d4e8d2d3b2a5d16b80b3a23f2
Bastien Léonard <bastien.leonard@gmail.com>
2012-03-05 10:57:01 +0100

    Made the events example more visual

--- examples/events.py
+++ examples/events.py
@@ -4,24 +4,53 @@
 import sf
 
 
+class EventQueue(object):
+    def __init__(self, max_events):
+        self.max_events = max_events
+        self.events = []
+
+    def add(self, event):
+        self.events.append(event)
+
+        if len(self.events) > self.max_events:
+            self.events.pop(0)
+
+    # We could make it a custom drawable, but I personally don't like it
+    def draw_(self, window):
+        y = 0
+        text = sf.Text('Last events:')
+        text.character_size = 15
+        text.color = sf.Color.BLACK
+        window.draw(text)
+
+        for event in reversed(self.events):
+            y += text.global_bounds.height
+            text.string = str(event)
+            text.color = sf.Color.BLACK
+            text.y = y
+            window.draw(text)
+
+
 def main():
-    window = sf.RenderWindow(sf.VideoMode(640, 480), 'Events example')
+    window = sf.RenderWindow(sf.VideoMode(800, 600), 'SFML Events example')
     window.framerate_limit = 60
-    window.clear(sf.Color.WHITE)
-    window.display()
     running = True
+    events = EventQueue(50)
 
     while running:
-        for event in window.iter_events():
-            # Print all events
-            print(event)
-
+        for event in window.iter_events(): 
             # Stop running if the application is closed
             # or if the user presses Escape
             if (event.type == sf.Event.CLOSED or
                (event.type == sf.Event.KEY_PRESSED and
                 event.code == sf.Keyboard.ESCAPE)):
                 running = False
+
+            events.add(event)
+
+        window.clear(sf.Color.WHITE)
+        events.draw_(window)
+        window.display()
 
     window.close()
 
